a50405a5d378ac87f6b6eb17ee3dba2f91bc7175
dragosbadswizz <dragos.balan@adswizz.com>
2020-06-01 11:14:49 +0300

    list user added

--- src/main/java/com/example/webcv/controllers/UserController.java
+++ src/main/java/com/example/webcv/controllers/UserController.java
@@ -3,19 +3,34 @@ package com.example.webcv.controllers;
 import com.example.webcv.entities.User;
 import com.example.webcv.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.function.EntityResponse;
+
+import javax.xml.ws.Response;
 
 @RestController
+@RequestMapping(path = "/webcv")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @RequestMapping (path = "/users", method = RequestMethod.GET)
-    public Iterable<User> retrieveUsers(){
-        return userService.retrieveAll();
+    public ResponseEntity<Iterable<User>> retrieveUsers(){
+        return new ResponseEntity<>(userService.retrieveAll(), HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/user/{id}")
+    public ResponseEntity<User> retrieveUser(@PathVariable(name = "id") Integer userId){
+        User user = userService.retrieveUser(userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.CONFLICT)); // returning 409 just for the sake of doing it
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/webcv/services/UserService.java
+++ src/main/java/com/example/webcv/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,6 +18,10 @@ public class UserService {
 
     public Iterable<User> retrieveAll() {
         return userRepository.findAll();
+    }
+
+    public Optional<User> retrieveUser(Integer userId){
+        return userRepository.findById(userId);
     }
 
 }
